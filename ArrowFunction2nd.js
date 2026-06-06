c55bad7fa964edf5f616f1afda5a0b79fb44826b
Alok Paul <alokpaul369.com>
2026-06-06 18:07:00 +0600

    3rd

---
+++ ArrowFunction2nd.js
@@ -0,0 +1,13 @@
+const vowels=(str)=>{
+        let count=0;
+    for (const char of str)
+    {
+        if (char === "a" || char === "e" || char === "i" || char === "o" || char === "u")
+        {
+            count++;
+        }
+    }
+    return count;
+}
+str=prompt("Enter a string:");
+console.log(vowels(str));
