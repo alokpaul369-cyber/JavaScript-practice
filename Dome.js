3cf70d5e6062a7944b6d39ee6d7a300a1f03bf0e
Alok Paul <alokpaul369.com>
2026-06-13 10:46:16 +0600

    5th

---
+++ Dome.js
@@ -0,0 +1,7 @@
+let box = document.querySelectorAll(".Box");
+box[0].innerText = "new value 0";
+box[1].innerText = "new value 1";
+box[2].innerText = "new value 2";
+console.log(box[0]);
+console.log(box[1]);
+console.log(box[2]);
